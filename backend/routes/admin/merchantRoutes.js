33f01f7c57bc28ebb84f259b0ffd97b5ccc7f6c1
minsway01 <minsway01@gmail.com>
2026-05-29 10:34:40 +0530

    check the issuse

--- backend/routes/admin/merchantRoutes.js
+++ backend/routes/admin/merchantRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const controller = require('../../controllers/admin/merchantController');
+const controller = require('../../controllers/admin/MerchantController');
 
 const upload = require('../../middleware/upload');
 
