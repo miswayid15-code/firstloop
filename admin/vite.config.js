c2ed3a55249435115d2e78a4929be245c146f6bd
minsway01 <minsway01@gmail.com>
2026-06-19 20:04:10 +0530

    Feat vite issuse

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    server: {
-  allowedHosts: ['firstpassapp.co', 'www.firstpassapp.co']
-}
+    allowedHosts: [
+      'firstpassapp.co',
+      'www.firstpassapp.co'
+    ]
   }
 })
