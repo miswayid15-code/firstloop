8024086ef73c1be942e296c2d04565b535197dba
minsway01 <minsway01@gmail.com>
2026-07-03 11:24:36 +0530

    Feat new migartion

--- backend/migrations/20260630052757-alter-user_id.js
+++ backend/migrations/20260630052757-alter-user_id.js
@@ -5,10 +5,8 @@ module.exports = {
     await queryInterface.addColumn('receptionists', 'rep_id', {
       type: Sequelize.STRING(250),
       allowNull: true,
-      after: 'id',
     });
-  },
-  async up(queryInterface, Sequelize) {
+
     await queryInterface.changeColumn('receptionists', 'email', {
       type: Sequelize.STRING,
       allowNull: true,
@@ -18,6 +16,7 @@ module.exports = {
 
   async down(queryInterface) {
     await queryInterface.removeColumn('receptionists', 'rep_id');
+
     await queryInterface.changeColumn('receptionists', 'email', {
       type: Sequelize.STRING,
       allowNull: false,
