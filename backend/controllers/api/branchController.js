ce5a5117a2e28661d6fc08b8d9b9868deabb0073
minsway01 <minsway01@gmail.com>
2026-07-10 19:21:14 +0530

    Feat lASTEST correction

--- backend/controllers/api/branchController.js
+++ backend/controllers/api/branchController.js
@@ -1135,8 +1135,8 @@ exports.branch_id = async (req, res) => {
                 ? data.profile_image
                 : data.pending_profile_image;
 
-        data.profile_image = profileImagePath
-            ? `${baseUrl}/${profileImagePath.replace(/\\/g, '/')}`
+        data.profile_image = data.pending_profile_image
+            ? `${baseUrl}/${data.pending_profile_image.replace(/\\/g, '/')}`
             : null;
         if (data.BranchImages) {
             data.BranchImages = data.BranchImages.map(img => {
@@ -1144,8 +1144,8 @@ exports.branch_id = async (req, res) => {
 
                 return {
                     id: img.id,
-                    image: imagePath
-                        ? `${baseUrl}/${imagePath.replace(/\\/g, '/')}`
+                    image: img.pending_image
+                        ? `${baseUrl}/${img.pending_image.replace(/\\/g, '/')}`
                         : null,
                     image_status: img.image_status,
                     rejected_reason: img.rejected_reason
@@ -1160,8 +1160,8 @@ exports.branch_id = async (req, res) => {
 
                 return {
                     id: img.id,
-                    image: imagePath
-                        ? `${baseUrl}/${imagePath.replace(/\\/g, '/')}`
+                    image: img.pending_image
+                        ? `${baseUrl}/${img.pending_image.replace(/\\/g, '/')}`
                         : null,
                     image_status: img.image_status,
                     rejected_reason: img.rejected_reason
