4b4d595a23c57751f4e6108d61324a89334bb05d
minsway01 <minsway01@gmail.com>
2026-07-20 18:01:23 +0530

    Feat Update the country code

--- backend/controllers/api/CustomerController.js
+++ backend/controllers/api/CustomerController.js
@@ -53,7 +53,7 @@ exports.register = async (req, res) => {
             profile_image
         } = req.body;
 
-
+console.log("body",req.body)
         let phoneNumber;
         let nationalNumber;
         let callingCode;
@@ -137,7 +137,7 @@ exports.register = async (req, res) => {
         // profile image upload
         let profileImage = '';
 
-        // console.log("FILES:", req.files);
+        console.log("FILES:", req.files);
 
         if (req.files && req.files.length > 0) {
 
