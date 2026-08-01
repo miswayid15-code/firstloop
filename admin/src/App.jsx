010d9c577494c1621bda7e7321ca3ec5e27deae9
minsway01 <minsway01@gmail.com>
2026-08-01 18:55:37 +0530

    first commit Admin Chart

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -54,6 +54,7 @@ import Conditions from './page/TermsCustomer.jsx';
 import HowItWorks from './page/HowItWorks.jsx';
 import DeleteAccount from './page/DeleteAccount.jsx';
 import UnderConstruction from './page/UnderConstruction.jsx';
+import CustomerApp from './page/CustomerApp.jsx';
 
 function ScrollToTopAndAnimate() {
   const { pathname } = useLocation();
@@ -89,7 +90,7 @@ function ScrollToTopAndAnimate() {
 
 function WebsiteLayout() {
   const { pathname } = useLocation();
-  const hideHeaderFooter = ['/under-construction', '/delete-account', '/data-policy', '/conditions'].includes(pathname);
+  const hideHeaderFooter = ['/under-construction', '/delete-account', '/data-policy', '/conditions', '/customer-app'].includes(pathname);
 
   const [loading, setLoading] = useState(!hideHeaderFooter);
   const [fade, setFade] = useState(false);
@@ -139,6 +140,7 @@ function WebsiteLayout() {
           <Route path="/conditions" element={<Conditions />} />
           <Route path="/delete-account" element={<DeleteAccount />} />
           <Route path="/under-construction" element={<UnderConstruction />} />
+          <Route path="/customer-app" element={<CustomerApp />} />
         </Routes>
       </main>
 
