8f0ed123738ef32036b853e7464bff7dad116f0d
minsway01 <minsway01@gmail.com>
2026-09-07 20:12:34 +0530

    Feat Update Correction

--- admin/inspect_card.js
+++
@@ -1,8 +0,0 @@
-import { fetchCardData } from './api/utils/fetchCardData.js';
-
-async function checkCard() {
-    const data = await fetchCardData({ id: 35, type: 1, cus_id: 1 });
-    console.log('API returned card details:', JSON.stringify(data, null, 2));
-}
-
-checkCard();
