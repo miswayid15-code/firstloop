e2e462fdb9ca4ce4abbf23769bf8dd10886a6422
minsway01 <minsway01@gmail.com>
2026-09-13 03:43:28 +0530

    Feat Update the Admin

--- backend/migrations/20260912095359-add-month-to-stamp-card.js
+++ backend/migrations/20260912095359-add-month-to-stamp-card.js
@@ -1,19 +1,27 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('stamp_cards', 'month', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    });
-    await queryInterface.addColumn('stamp_cards', 'qr_color', {
-        type: Sequelize.STRING(255),
-      allowNull: true,
-    });
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.addColumn('stamp_cards', 'month', {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+        });
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('stamp_cards', 'month');
-    await queryInterface.removeColumn('stamp_cards', 'qr_color');
-  },
+        await queryInterface.addColumn('stamp_cards', 'qr_color', {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        const table = await queryInterface.describeTable('stamp_cards');
+
+        if (table.month) {
+            await queryInterface.removeColumn('stamp_cards', 'month');
+        }
+
+        if (table.qr_color) {
+            await queryInterface.removeColumn('stamp_cards', 'qr_color');
+        }
+    },
 };
